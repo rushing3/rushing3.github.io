390af5050c38ca806b5c5f1c9a13df63d0d12731
Alex <arushing95@gmail.com>
2015-06-27 19:37:46 -0400

    Setup html and css

--- flaskapp.py
+++ flaskapp.py
@@ -1,13 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hey, Fariha! (:'
+    return render_template('index.html')
 
 @app.route('/fariha')
 def fariha():
-    return "You're purdy. How was breaking fast?"
+    return "Hey, Fariha! (:"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User {}'.format(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post {}'.format(post_id)
+
+@app.route('/projects/')
+def projects():
+    return 'Project page'
+
+@app.route('/about')
+@app.route('/about/<name>')
+def about(name=None):
+    return render_template('about.html', name=name)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        #do login
+        pass
+    else:
+        #show login form
+        pass
 
 if __name__ == '__main__':
         app.run(debug=True)
