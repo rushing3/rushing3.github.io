548a315b90d5466a25d04526fbc6eb099bdfbd38
Alex <arushing95@gmail.com>
2015-06-26 21:26:35 -0400

    More fariha

--- flaskapp.py
+++ flaskapp.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hey, Fariha! (:'
 
+@app.route('/fariha')
+def fariha():
+    return "You're purdy. How was breaking fast?"
+
 if __name__ == '__main__':
         app.run(debug=True)
