9a0fb448cc24524a0df4db965b60ec61b58e00d0
Alex <arushing95@gmail.com>
2015-07-03 11:47:42 -0400

    FACELIFT WITH BOOTSTRAP

--- flaskapp.py
+++ flaskapp.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('my_index.html')
+    return render_template('boot.html')
 
 @app.route('/fariha')
 def fariha():
