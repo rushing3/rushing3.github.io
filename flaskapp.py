5a06466b8847eb4dfd4ca98ff774dc402e937cb0
Alex <arushing95@gmail.com>
2016-04-13 23:14:02 -0400

    Added code to show resume(need to add Resume.pdf still)

--- flaskapp.py
+++ flaskapp.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def base():
     return render_template('base.html')
 
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
 if __name__ == '__main__':
         app.run(debug=True)
