5a45a3fe800303c9813d20329eb0217dc24163f1
Alex Rushing <arushing95@gmail.com>
2017-03-27 23:23:22 -0400

    Updated for CS1100

--- flaskapp.py
+++ flaskapp.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def base():
-    return render_template('base.html')
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 @app.route('/resume')
 def resume():
