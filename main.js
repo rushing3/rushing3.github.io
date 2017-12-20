c81731056cf8c3e2a15baaa93a111d5db1ee55e7
Alex Rushing <arushing95@gmail.com>
2017-12-20 16:55:57 -0600

    Remove templates dir

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var map = L.map("map",{center:[20.81509864599243, 18.3221435546875],zoom:2});
 var Esri_WorldTopoMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
 });
+
 Esri_WorldTopoMap.addTo(map);
 
 d3.csv("cities.csv",function(data){
